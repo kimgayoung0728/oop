1ef302ea5935fdbe3714fbab7f6c4225b0839c57
kimgayoung0728 <r.gl2565@yonsei.ac.kr>
2024-12-08 17:08:34 +0900

    Add battle page UI display function

--- src/main.cpp
+++ src/main.cpp
@@ -46,3 +46,35 @@ int getEffectiveness(const string& attackType, const string& defenderType) {
     }
     return 0; // Effective
 }
+
+// Display battle page
+void displayBattlePage(const Pokemon& p1, const Pokemon& p2, const string& latestSkill1, const string& latestSkill2, const string& effectiveness1, const string& effectiveness2, bool isPlayer1Turn) {
+    cout << "+-------------------------------------------------------------+\n";
+    cout << "| 2024-02 Object-Oriented Programming Pokemon Master          |\n";
+    cout << "+------------------------------+------------------------------+\n";
+    cout << "| " << setw(29) << left << p1.name + (isPlayer1Turn ? " (*)" : "")
+         << "| " << setw(29) << left << p2.name + (!isPlayer1Turn ? " (*)" : "") << "|\n";
+    cout << "| Type: " << setw(23) << left << p1.type
+         << "| Type: " << setw(23) << left << p2.type << "|\n";
+    cout << "| HP: " << setw(25) << left << p1.currentHP
+         << "| HP: " << setw(25) << left << p2.currentHP << "|\n";
+    cout << "+------------------------------+------------------------------+\n";
+    cout << "| Latest Skill: " << setw(15) << left << latestSkill1
+         << "| Latest Skill: " << setw(15) << left << latestSkill2 << "|\n";
+    cout << "| " << setw(29) << left << effectiveness1
+         << "| " << setw(29) << left << effectiveness2 << "|\n";
+    cout << "+------------------------------+------------------------------+\n";
+    for (int i = 0; i < 4; ++i) {
+        cout << "| (" << i << ") " << setw(25) << left << p1.skills[i].name
+             << "| (" << i << ") " << setw(25) << left << p2.skills[i].name << "|\n";
+        cout << "|     - Type: " << setw(17) << left << p1.skills[i].type
+             << "|     - Type: " << setw(17) << left << p2.skills[i].type << "|\n";
+        cout << "|     - Damage: " << setw(15) << left << p1.skills[i].damage
+             << "|     - Damage: " << setw(15) << left << p2.skills[i].damage << "|\n";
+        cout << "|     - Count: " << setw(16) << left << (to_string(p1.skills[i].remainingTry) + "(" + to_string(p1.skills[i].maxTry) + ")")
+             << "|     - Count: " << setw(16) << left << (to_string(p2.skills[i].remainingTry) + "(" + to_string(p2.skills[i].maxTry) + ")") << "|\n";
+        
+    
+    }
+    cout << "+------------------------------+------------------------------+\n";
+}
